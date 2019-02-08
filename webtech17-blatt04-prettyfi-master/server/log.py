79d973f861a9af2ccb012a8c12f529058eb88c55
JeremiasB <33179984+JeremiasB@users.noreply.github.com>
2019-02-08 13:03:08 +0100

    Deleting Extra Stuff

--- webtech17-blatt04-prettyfi-master/server/log.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Tobias'
-
-_log_level = 3  # from 0=None to 5=extremely verbose
-
-
-def log(level, msg):
-    global _log_level
-    if level <= _log_level:
-        print("[%d] %s" % (level, msg))
